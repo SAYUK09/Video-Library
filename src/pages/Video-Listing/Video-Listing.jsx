ef41e4b00db29e15d668fb20d2a5a45b0acf222b
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-05-18 22:03:09 +0530

    Fetched & Displayed Videos on video Listing Page

--- src/pages/Video-Listing/Video-Listing.jsx
+++ src/pages/Video-Listing/Video-Listing.jsx
@@ -1,9 +1,41 @@
-import React from "react"
+import React from "react";
+import "./Video-Listing.css";
+import { useVideos } from "../../contexts/videoLibContext";
+import { FaThumbsUp, FaRegClock, FaBorderNone } from "react-icons/fa";
 
 export function VideoListing() {
+  const { state, dispatch } = useVideos();
+
   return (
     <>
-      <h1>Video Listing</h1>
+      <div className="vidListingParent">
+        <div className="vidBody">
+          {state.videos.map((vidObj) => {
+            return (
+              <div key={vidObj._id} className="vidCardBody">
+                <div className="videoImgDiv">
+                  <img className="thumbnail-image" src={vidObj.thumbnail} />
+                </div>
+                <h4 className="video-title"> {vidObj.title}</h4>
+                <b>
+                  <small>{vidObj.channel}</small>
+                </b>
+                <small className="vid-category">{vidObj.category}</small>
+
+                <div className="vidBtnDiv">
+                  <button className="like-button" onClick={() => {}}>
+                    <FaThumbsUp className="like-icon" />
+                  </button>
+
+                  <button className="like-button" onClick={() => {}}>
+                    <FaRegClock className="like-icon" />
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
